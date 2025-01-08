82548ee962700030b7a14ad61e9fb13708443663
Ritikpanditonly <pandeyritik236@gmail.com>
2025-01-08 20:21:04 +0530

    add the file

---
+++ script.js
@@ -0,0 +1,63 @@
+// fundamental of javaScript;
+// arrays and object
+// functions return
+//async js coding
+// foreach map filter find indexof
+
+//FOREACH
+/*var arr = [1,2,3,5];
+arr.forEach(function(val){
+    console.log(val + "hello");
+})*/
+
+
+//MAP
+/*var arr = [1,2,3,4]
+var newarr = arr.map(function(val){
+    return 13;
+})*/
+
+//FILTER
+/*var arr = [1,2,3,4];
+var ans = arr.filter(function(val){
+    if(val >= 3){return true;}
+    else return false;
+})
+console.log (ans);*/
+
+//FIND
+/*var arr = [,2,3,5];
+var ans = arr.find(function(val){
+    if(val === 2)return val;
+})
+
+console.log(ans);*/
+
+// index of 
+//arr.indexOf()
+
+
+//OBJECTS 
+
+//object are key value pairs 
+
+/*var obj = {
+    name:"ritik",
+    age:12
+}
+
+Object.freeze(obj);
+
+obj.age= 25;*/
+
+ //FUNCTION
+/*function abcd(){
+    return 12;
+}
+var ans = abcd();*/
+
+//ASYNC 
+
+// LINE BY CODE CHALE ISEY SYNCHRONUS 
+// JO BHI CODE ASYNC NATURE KA HO USEY SIDE STACK MAI SEND KAR DO AGLE CODE KO CHALO JO BHI SYNC NATURE KA HO JAB BHI SARAA SYNC  CODE CHAL JAYE TAB CHECK KARO ASYNC CODE COMPLETE HUA KI NHI AGAR VO COMPLETE HUA HO TO USSEY MAIN STACK MEIN LAAO CHALA DO
+
